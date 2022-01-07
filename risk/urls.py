d78b29cd0b33ccda3bb216511ce42f4a4c0e33e1
Yekta Can Tursun <yektacan1999@gmail.com>
2022-01-07 12:23:39 +0300

    Registration added

--- risk/urls.py
+++ risk/urls.py
@@ -18,6 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', admin.site.urls),
+    #path('user/', admin.site.urls),
     path('user/', include('user.url')),
 ]
--- user/views.py
+++ user/views.py
@@ -1,19 +1,44 @@
 from django.shortcuts import render
 from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
-from knox.models import AuthToken
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import status
 from .serializers import UserSerializer, RegisterSerializer
-
+from rest_framework.decorators import api_view
 # Create your views here.
 # Register API
+class RegisterUser_test(GenericAPIView):
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'user': str(request.user),  # `django.contrib.auth.User` instance.
+            'auth': str(request.auth),  # None
+        }
+        return Response(content,status=status.HTTP_202_ACCEPTED)
+
+
 class RegisterUser(GenericAPIView):
     serializer_class = RegisterSerializer
 
+    def get(self, request):
+        return Response("OK")
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         return Response({
         "user": UserSerializer(user, context=self.get_serializer_context()).data,
-        "token": AuthToken.objects.create(user)[1]
         })
+
+@api_view(["GET","POST"])
+def product_list(request):
+    if request.method == "GET":
+       
+        return Response("ok")
+    elif request.method == "POST":
+
+        
+        return Response("ok",status=status.HTTP_201_CREATED)
